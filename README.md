c5d66406dc2a5abde0c35cebdd177e49d0eb5a51
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-09 16:49:13 -0700

    create pig-latin translator
    Co-Authored-by: Erika Debelis <erika.debelis@gmail.com> John Blalock <John Blalock <simpledimplejohn@gmail.com> Kody Gordon <gordon.km89@gmail.com>

---
+++ README.md
@@ -0,0 +1,61 @@
+Describe: vowelFinder();
+
+Test: "It recognizes a single vowel."
+Code: vowelFinder("a");
+Expected Output: 1
+
+Test: "It recognizes a single vowel regardless of case."
+Code: vowelFinder("A");
+Expected Output: 1
+
+Test: "It ignores non-alphabetical characters since they can't be vowels."
+Code: vowelFinder("*&$92%");
+Expected Output: 0
+
+Test: "It recognizes words beginning with a vowel"
+Code: vowelFinder("okay");
+Expected Output: 1
+
+Test: "Function adds "way" to end of word"
+Code: vowelFinder("okayway");
+Expected Output: 1
+
+Describe: consonateFinder();
+
+Test: "It recognizes words beginning with a consonant"
+Code: consonateFinder("cow");
+Expected Output: 1
+
+Test: "Function moves consonates to end of word"
+Code: consonateFinder("owc");
+Expected Output: 1
+
+Test: "Function add's 'ay' to end"
+Code: consonateFinder("owcay");
+Expected Output: 1
+
+Describe: quFinder();
+
+Test: "It recognizes words that have qu at the beginning of the word"
+Code: quFinder("queen");
+Expected Output: 1
+
+Test "Function moves 'qu' to the end of word"
+Code: quFinder("eenqu); 
+Expected Output: 1
+
+Test "Function add's 'ay' to the end of the word"
+Code: quFinder("eenquay);
+Expected Output: 1
+
+
+
+grotesque
+quegrotes
+otesquegray via pig latin translator
+
+rotesquegay
+
+squeal
+uealsqay
+e 
---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+//Utlilty Logic
+const vowelArray = ["a", "e", "i", "o", "u"];
+let text = "a";
+let inputForm = $("#sentence").val();
+const consonantsArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p","q", "r", "s", "t", "v", "w", "x", "y", "z"];
+
+
+
+
+function vowelFinder(text, vowelArray) {
+  for (let i = 0; i < vowelArray.length; i++) {
+    if (text.toLowerCase() === vowelArray[i].toLowerCase()) {
+      return i;
+    };
+    return -1
+    if (!elephant(text)) {
+      
+      return false
+    }
+  }
+  
+} 
+
+
+
+let text = [];
+// inputForm.includes("[^a-zA-Z]", "");
+text = inputForm.split(" ");
+function wayAdder(text, vowelArray) {
+  for (let i = 0; i < vowelArray.length; i++) {
+    if (text.charAt(0) === vowelArray[i]) {
+      return (text + "way");
+    } else {
+      return false;
+    }
+  }
+}
+
+else if (text.charAt(0) === consonantsArray[i]) {
+  constText = text.split(" "); //"m" "o" "v" "e"
+  if (constText.charAt(0) === consonantsArray[i]) && (constText.charAt(1) === vowelArray[i]) //move 
+
+  let constPig = constText.slice(0) //"o" "v" "e"
+  let newPigLatinWord = constPig.concat(constText(0)) //"o" "v" "e" "m"
+  let pigLatinConst = newPigLatinWord.join //ovem
+    return (pigLatinConst + "ay") //ovemay
+}
+  if (constText.charAt(0) === consonantsArray[i]) && (constText.charAt(1) === consonantsArray[i]) //create
+
+  let constPig = constText.slice(1) //"c" "r" "e" "a" "t" "e"
+  let newPigLatinWord = constPig.concat(constText(1)) //"e" "a" "t" "e" "c" "r"
+  let pigLatinConst = newPigLatinWord.join // eatecr
+    return (pigLatinConst + "ay") // eatecray
+}
+
+  if (constText.charAt(0) === "q") && (constText.charAt(1) === "u") // "q" "u" "i" "c" "k"
+
+  let constPig = constText.slice(1) //"i" "c" "k"
+  let newPigLatinWord = constPig.concat(constText(1)) //"i" "c" "k" "q" "u"
+  let pigLatinConst = newPigLatinWord.join //ickqu
+    return (pigLatinConst + "ay") //ickquay
+}
+  if (constText.charAt(0) === consonantsArray[i]) && (constText.charAt(1) === "q") && (constText.charAt(1) === "u") // "s" "q" "u" "e" "a" "k"
+
+  let constPig = constText.slice(2) // "e" "a" "k" "s" "q" "u"
+  let newPigLatinWord = constPig.concat(constText(2)) // "e" "a" "k" "s" "q" "u"
+  let pigLatinConst = newPigLatinWord.join //eaksqu
+    return (pigLatinConst + "ay") //eaksquay
+
+else if ()
+
+
+//Doesn't make sense?below//
+//let inputForm = vowelArray.split(" ");
+
+
+
+//Business Logic
+
+
+
+
+
+
+
+
+
+
+// UI Logic
